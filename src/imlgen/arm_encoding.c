faca899f64e7124ca971ad90304062a04d61b974
torph <73564623+Torphedo@users.noreply.github.com>
2025-03-27 23:36:45 -0400

    Some decoding cleanup

--- src/imlgen/arm_encoding.c
+++ src/imlgen/arm_encoding.c
@@ -1,7 +1,12 @@
 #include "arm_encoding.h"
 #include "bitmanip.h"
 
-// A lookup table entry used for instruction decoding
+// TODO: Make this file C++ and all the functions constexpr so they can be
+// tested via static_assert().
+
+// A lookup table entry used for instruction decoding.
+// The ARM spec uses a compact bitmask notation where 'x' means the bit's value
+// doesn't matter. We have to use 2 bitmasks to recreate that behaviour.
 typedef struct {
     // Instruction will be ANDed with this mask
     u32 mask;
@@ -11,9 +16,23 @@ typedef struct {
     u32 group;
 }arm_page_entry;
 
+// Lookup table entry to check 2 values against 2 bitmasks.
+typedef struct {
+    // First value will be ANDed with this mask and compared with the pattern
+    u32 mask1;
+    u32 pattern1;
+
+    // Same but for the second value
+    u32 mask2;
+    u32 pattern2;
+
+    // If it matches both patterns, the instruction belongs to this group.
+    u32 group;
+}arm_double_page_entry;
+
 // See pg. C4-192. This lookup table encodes the bit patterns to compare op0
 // against, and the resulting instruction page if they match.
-arm_page_entry top_instr_pagetable[] = {
+static const arm_page_entry top_instr_pagetable[] = {
     {0b1110, 0b1000, L1_DATA_IMM},
     {0b1110, 0b1010, L1_BRANCH},
     {0b0101, 0b0100, L1_LD_STR},
@@ -39,7 +58,7 @@ L1_page instr_get_group(u32 instr) {
 }
 
 // See C4.2 on pg. C4-193
-arm_page_entry data_imm_pagetable[] = {
+static const arm_page_entry data_imm_pagetable[] = {
     {0b110, 0b000, L2_DATA_IMM_PC_REL_ADDR},
     {0b110, 0b010, L2_DATA_IMM_ADDSUB_IMM},
     {0b111, 0b100, L2_DATA_IMM_LOGICAL_IMM},
@@ -49,7 +68,7 @@ arm_page_entry data_imm_pagetable[] = {
 };
 
 L2_data_imm_page data_imm_get_group(u32 instr) {
-    const u8 op0 = ((instr & (0b111 << 23)) >> 23);
+    const u8 op0 = GET_BIT_REGION(instr, 23, 25);
 
     for (u32 i = 0; i < ARRAY_SIZE(data_imm_pagetable); i++) {
         const arm_page_entry entry = data_imm_pagetable[i];
@@ -62,41 +81,34 @@ L2_data_imm_page data_imm_get_group(u32 instr) {
     return L2_DATA_IMM_UNALLOCATED;
 }
 
-// See section C4.3 on pg. C4-197 for a table defining all these values & cases
+// See section C4.3 on pg. C4-197 for the table where all these values come from.
+static const arm_double_page_entry branch_pagetable[] = {
+    {0b111, 0b010, 0b1000, 0b0000, L2_BRANCH_CONDITIONAL},
+
+    {0b111, 0b110, 0b1100, 0b0000, L2_BRANCH_EXCEPTION},
+    {0b111, 0b110, 0b1111, 0b0100, L2_BRANCH_SYSTEM},
+
+    {0b111, 0b110, 0b1000, 0b1000, L2_BRANCH_UNCONDITIONAL_REG},
+
+    {0b011, 0b000, 0b0000, 0b0000, L2_BRANCH_UNCONDITIONAL_IMM},
+    {0b011, 0b001, 0b1000, 0b0000, L2_BRANCH_COMPARE},
+    {0b011, 0b001, 0b1000, 0b1000, L2_BRANCH_TEST},
+};
+
 L2_branch_page branch_get_group(u32 instr) {
     const u8 op0 = GET_BIT_REGION(instr, 29, 31);
     const u8 op1 = GET_BIT_REGION(instr, 22, 25);
 
-    // TODO: We can probably turn this into a bitmask loop like above by using
-    // 2 mask arrays
-    if (op0 == 0b010) {
-        if ((op1 & 0b1000) == 0) {
-            return L2_BRANCH_CONDITIONAL;
-        }
-    }
-    else if (op0 == 0b110) {
-        if ((op1 & 0b1100) == 0) {
-            return L2_BRANCH_EXCEPTION;
-        }
-        else if (op1 == 0b0100) {
-            return L2_BRANCH_SYSTEM;
-        }
-        else if ((op1 & 0b1000) == 0b1000) {
-            return L2_BRANCH_UNCONDITIONAL_REG;
-        }
-    }
-    else if ((op0 & 0b011) == 0) {
-        return L2_BRANCH_UNCONDITIONAL_IMM;
-    }
-    else if ((op0 & 0b011) == 0b01) {
-        if (op0 & 0b1000) {
-            return L2_BRANCH_COMPARE;
-        } else {
-            return L2_BRANCH_TEST;
+    for (u32 i = 0; i < ARRAY_SIZE(branch_pagetable); i++) {
+        const arm_double_page_entry entry = branch_pagetable[i];
+        if ((op0 & entry.mask1) == entry.pattern1) {
+            if ((op1 & entry.mask2) == entry.pattern2) {
+                return entry.group;
+            }
         }
     }
 
-    // No match, instruction is invalid
+    // There was no matching pattern, instruction is invalid.
     return L2_BRANCH_UNALLOCATED;
 }
 
